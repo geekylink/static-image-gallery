57304e2170664b661d7ffb97ef8c8a136897ebe8
James Danielson <geekylink@github.io>
2023-02-22 17:16:21 -0500

    Moved core logic out of the Albums component

--- src/App.tsx
+++ src/App.tsx
@@ -7,37 +7,62 @@ import { Albums } from "./components";
 import {getAlbumPath} from "./components/Albums/Albums.functions";
 
 
+const GALLERY_TITLE = "Travel Pictures";
+
+
 function App() {
 
   const [imagePath, setImagePath] = useState("");
-  const [pastAlbumList, setPastAlbumList] = useState(undefined);
+  const [albumList, setAlbumList] = useState([]);
+
+  const album = albumList[albumList.length-1]; // Current level of album
 
   return (
     <div className="App">
       <div>
+        <div>
+          {(album != null) /* Album Name */
+                  ? <>
+                      <h2>{(album.type == "dir") 
+                          ? <>Album: {album.file}</>
+                          : <>{album.file}</>
+                        }
+                      </h2><br />
+                      <button onClick={() => {
+                          setImagePath("");
+
+                          let newAlbumList = albumList.concat();
+                          newAlbumList.pop();
+                          console.log(newAlbumList);
+                          setAlbumList(newAlbumList);
+                        }}
+                      >
+                        Back
+                      </button>
+                      <button onClick={() => {
+                        setImagePath("");
+                        setAlbumList([]);
+                        }}
+                      >
+                        Home
+                      </button>
+                    </>
+                  : <h2>{GALLERY_TITLE}</h2>
+          }
+        </div>
+
         {(imagePath === "")
           ? <Albums OnImageClick={(albumList) => {
-              setImagePath(getAlbumPath(albumList));
-              setPastAlbumList(albumList);
-            }} startAlbumList={pastAlbumList} />
+                setImagePath(getAlbumPath(albumList));
+                setAlbumList(albumList);
+              }} 
+              albumList={albumList} 
+              OnAlbumClick={(file) => {
+                setAlbumList(albumList.concat(file));
+              }}
+            />
           :
           <>
-            <button onClick={() => {
-              setImagePath("");
-
-              let newAlbumList = pastAlbumList.concat();
-              newAlbumList.pop();
-              console.log(newAlbumList);
-              setPastAlbumList(newAlbumList);
-            }}>
-              Back
-            </button>
-            <button onClick={() => {
-              setImagePath("");
-              setPastAlbumList(undefined);
-            }}>
-              Home
-            </button>
             <br />
             <a href={`${imagePath}`} target="_blank" rel="noopener noreferrer"><img src={`${imagePath}`} width={"100%"} /></a>
           </>
--- src/components/Albums/Albums.tsx
+++ src/components/Albums/Albums.tsx
@@ -4,29 +4,32 @@ import FileTree from "../../../data/image_meta.json"
 
 import {getAlbumPath} from "./Albums.functions";
 
+//import {ButtonBar} from "../";
+
 type AlbumsProps = {
     //OnImageClick: (imagePath: string) => void;
     OnImageClick?: (albumList: any) => void;
-    startAlbumList?: any;
+    OnAlbumClick?: (file: any) => void;
+    albumList: any;
 };
 
 export const Albums = ({
-    OnImageClick,
-    startAlbumList
+    OnImageClick = () => {},
+    OnAlbumClick = () => {},
+    albumList
 }: AlbumsProps) => {
 
-    const [pastAlbumList, setPastAlbumList] = useState(((startAlbumList === undefined) ? [] : startAlbumList));
-    const album = pastAlbumList[pastAlbumList.length-1];
+    const album = albumList[albumList.length-1];
 
     // Build the album images path
-    let albumPath = getAlbumPath(pastAlbumList);
+    let albumPath = getAlbumPath(albumList);
 
     // Either the root directory or a sub directory
-    let albumList = (album == null) ? FileTree.files.tree : album.subTree;
+    let albumTree = (album == null) ? FileTree.files.tree : album.subTree;
 
     // Does this album have sub albums?
     let albumHasSubs = false;
-    albumList.forEach((album) => {
+    albumTree.forEach((album) => {
         if (album.type === "dir") {
             albumHasSubs = true;
         }
@@ -34,37 +37,15 @@ export const Albums = ({
 
     return (<>
         <div>
-            {(album != null) /* Album Name */
-                ? <>
-                    <h2>Album: {album.file}</h2><br />
-
-                    <button onClick={() => { /* Back Button */
-                        let newAlbumList = pastAlbumList.concat();
-                        newAlbumList.pop();
-                        setPastAlbumList(newAlbumList);
-                    }}>
-                        Back
-                    </button>
-                    {(pastAlbumList.length > 1) 
-                        ? 
-                        <button onClick={() => {setPastAlbumList([]); }}>
-                            Home
-                        </button>
-                        : null
-                    }
-                  </>
-                : null
-            }
-
-            <div><br />
+            <div>
                 {(albumHasSubs) /* List sub albums */
                     ? <><h3>Albums:</h3><br /></>
                     : null
                 }
-                {albumList.map((file, key) => (
+                {albumTree.map((file, key) => (
                     ((file.type === "dir") 
                         ? <button onClick={() => {
-                            setPastAlbumList(pastAlbumList.concat(file));
+                            OnAlbumClick(file);
                         }}>{file.file}</button>
                         :
                         null
@@ -73,10 +54,10 @@ export const Albums = ({
             </div>
             
             <div>
-                {albumList.map((file, key) => (
+                {albumTree.map((file, key) => (
                     ((file.type === "image")
                         ? <>
-                            <a onClick={() => {OnImageClick(pastAlbumList.concat(file));}}><img src={`${albumPath}/${file.file}`} width={"30%"} /></a>&nbsp;&nbsp;
+                            <a onClick={() => {OnImageClick(albumList.concat(file));}}><img src={`${albumPath}/${file.file}`} width={"30%"} /></a>&nbsp;&nbsp;
                         </>
                         :
                         null
